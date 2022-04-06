392139e6fb8e5085b8fc8c50e267e79d4ba0294e
Hardik Pithadiya <pithadiyahardik646@gmail.com>
2022-04-06 21:19:10 +0530

    Added footer

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,6 +6,7 @@ import Carousel from "../../components/Carousel/Carousel";
 import AboutUs from "../../components/AboutUs/AboutUs";
 import Rooms from "../../components/Rooms/Rooms";
 import ContactUs from "../../components/ContactUs/ContactUs";
+import SimpleReactFooter from "simple-react-footer";
 // import AnimatedComponent from "../components/Animated-component/AnimatedComponent";
 import AnimatedComponent from "../../components/Animated-component/AmimatedComponent"
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,7 +15,53 @@ import AnimatedComponent from "../../components/Animated-component/AmimatedCompo
 
 import "./homepage.styles.scss";
 
-const HomePage = () => (
+const HomePage = () => 
+{
+  const description = "EZ-Masala has revolutionized the current way of cooking in the world of Indian kitchens. For this, no long-life expertise is required to make vegetables in the kitchen. You become a super chef of your home on your own for a lifetime just by watching the video once for all you have to make."
+  const title = "E-Z Masala";
+  const columns = [
+    {
+        title: "Contact Details",
+        resources: [
+            {
+                name: "Mobile\n9910 387 487",
+                link:"#"
+            },
+            {
+              name: "ezmasala.it@gmail.com",
+              link:"#"
+          }
+        ]
+    },
+    {
+        title: "Legal",
+        resources: [
+            {
+                name: "Privacy",
+                link: "#"
+            },
+            {
+                name: "Terms",
+                link: "#"
+            }
+        ]
+    },
+    {
+        title: "Location",
+        resources: [
+            {
+                name: `B1-337, First Floor, Janakpuri
+                New Delhi - 110058`,
+                link: "#"
+            },
+            
+        ]
+    }
+ ];
+
+
+return (
+
   // <div className="homepage">
   //   <Directory />
   // </div>
@@ -27,12 +74,29 @@ const HomePage = () => (
           <AboutUs />
           <Rooms />
           <ContactUs />
+          
           </center>
+          <SimpleReactFooter 
+    description={description} 
+    title={title}
+    columns={columns}
+    linkedin="fluffy_cat_on_linkedin"
+    facebook="fluffy_cat_on_fb"
+    twitter="fluffy_cat_on_twitter"
+    instagram="fluffy_cat_live"
+    youtube="UCFt6TSF464J8K82xeA?"
+    pinterest="fluffy_cats_collections"
+    copyright="black"
+    iconColor="black"
+    backgroundColor="bisque"
+    fontColor="black"
+    copyrightColor="darkgrey"
+ />
 				
 			</div>
   </>
 
 
 );
-
+}
 export default HomePage;
